61b7596bb4874fa3e03adb4f79a34cf4017f155e
developer <arman.zohrabyan.96@gmail.com>
2018-10-10 17:14:25 +0300

    separated webpack file

---
+++ webpack.client.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
+const browserConfig = {
+  entry: './src/browser/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "true"
+    })
+  ]
+};
+
+module.exports = browserConfig;
