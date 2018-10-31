39a7402dbee313fe9ad9e0c0b0a03d0dcde13bca
developer <arman.zohrabyan.96@gmail.com>
2018-10-31 19:02:31 +0300

    added language receipt

--- server/customMiddlewares/RequestHandlers.js
+++ server/customMiddlewares/RequestHandlers.js
@@ -1,8 +1,18 @@
 import { matchPath } from 'react-router-dom';
+import geoip from 'geoip-lite';
+import requestIp from 'request-ip';
 import routes from '../../src/routes';
 
 class RequestHandlers {
   static getActiveRoute = (req) => routes.find((route) => matchPath(req.url, route)) || {}
+  static getLanguage = (req) => {
+    const userIp = requestIp.getClientIp(req);
+    const geo = geoip.lookup(userIp) || {};
+    if (geo.country) {
+      return geo.country.toLowerCase();
+    }
+    return 'ru';
+  }
 }
 
 export default RequestHandlers;
--- server/customMiddlewares/index.js
+++ server/customMiddlewares/index.js
@@ -2,6 +2,7 @@ import RequestHandlers from './RequestHandlers';
 
 const customMiddlewares = (req, res, next) => {
   res.locals.activeRoute = RequestHandlers.getActiveRoute(req);
+  res.locals.language = RequestHandlers.getLanguage(req);
 
   next();
 };
