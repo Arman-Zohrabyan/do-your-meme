51254bcfe8d6db8d1c97caf858b31016048f8bac
Arman-Zohrabyan <arman.zohrabyan.96@gmail.com>
2018-11-18 14:21:24 +0300

    fixed Language not defined bug

--- public/2.bundle.js
+++ public/2.bundle.js
@@ -8,9 +8,11 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (immutable) */ __webpack_exports__["default"] = Fb;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__classes_Language__ = __webpack_require__(21);
+
 
 function Fb() {
-  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", null, Language.take('underDevelopment'));
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", null, __WEBPACK_IMPORTED_MODULE_1__classes_Language__["a" /* default */].take('underDevelopment'));
 }
 
 /***/ })
--- src/components/Fb.js
+++ src/components/Fb.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Language from '../classes/Language';
 
 export default function Fb () {
   return (
